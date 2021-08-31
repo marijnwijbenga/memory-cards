b6015489766b4e42a9df91d60ab9c9ec0d46a379
Marijn Wijbenga(home) <m.wijbenga@outlook.com>
2021-08-31 16:00:45 +0200

    refactoring the modal component
    refactoring the modal component

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
 })
-export class ModalComponent {}
+export class ModalComponent {
+  @Input() turns: number = 0;
+  @Input() score: number = 0;
+  @Output() newGame: EventEmitter<void> = new EventEmitter();
+
+  onNewGame(): void {
+    this.newGame.emit();
+  }
+}
